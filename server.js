b3e54935e2560ad78eaa8708367362411b65023f
Christian Laguador <christianlaguadorr@gmail.com>
2026-07-04 18:21:11 +0400

    setting up express server with Supabase client connection

---
+++ server.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_PUBLISHABLE_KEY);
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
